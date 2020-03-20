80784161fa23ad839912bb8484c14c80af7cf94e
Athiruj Poositaporn <athiruj.poositaporn@gmail.com>
2020-03-21 02:07:42 +0700

    create buttons

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -8,12 +8,32 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
+    var questions = [
+      "What's your fav color?",
+      "What's your fav animal?",
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My first app'),
         ),
-        body: Text('This is body'),
+        body: Column(
+          children: [
+            Text("Question!"),
+            RaisedButton(
+              child: Text('Ans 1'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Ans 2'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Ans 3'),
+              onPressed: null,
+            ),
+          ],
+        ),
       ),
     );
   }
