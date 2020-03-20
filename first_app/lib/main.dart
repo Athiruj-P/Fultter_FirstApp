2196667d4eacb9b6b15c7b7255b7439ee9bd84b6
Athiruj Poositaporn <athiruj.poositaporn@gmail.com>
2020-03-21 02:44:48 +0700

    stateless

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -6,7 +6,27 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionsIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      // เป็นฟังชันที่บังคับให้ flutter render ui ใหม่
+      //เพื่อบอกให้ MyApp รู้ว่าถ้าสิ่งนี้เปลี่ยน ให้ render ใหม่
+      questionsIndex += 1;
+    });
+    print(questionsIndex);
+  }
+
+  @override
   Widget build(BuildContext context) {
     var questions = [
       "What's your fav color?",
@@ -19,18 +39,22 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text("Question!"),
+            Text(
+              questions[questionsIndex],
+            ),
             RaisedButton(
               child: Text('Ans 1'),
-              onPressed: null,
+              onPressed: answerQuestion,
             ),
             RaisedButton(
               child: Text('Ans 2'),
-              onPressed: null,
+              onPressed: () => print("Ans 2 chosen"),
             ),
             RaisedButton(
               child: Text('Ans 3'),
-              onPressed: null,
+              onPressed: () {
+                print("Ans 3 chosen!");
+              },
             ),
           ],
         ),
